637e8bd55d39bbd27322066c01dcd1742ab2765c
Kevin Kelly <kvinklly@gmail.com>
2020-06-20 08:18:09 -0500

    Replace tabs with spaces

--- src/components/AppButton.vue
+++ src/components/AppButton.vue
@@ -1,57 +1,57 @@
 <template>
-	<button :style="styles" @click="handleClick">
-		<!-- @slot Use this slot to place the button content -->
-		<slot></slot>
-	</button>
+  <button :style="styles" @click="handleClick">
+    <!-- @slot Use this slot to place the button content -->
+    <slot></slot>
+  </button>
 </template>
 
 <script>
 import sizeMixin from './sizeMixin'
 export default {
-	name: 'app-button',
-	mixins: [sizeMixin],
-	props: {
-		/**
-		 * Sets the button font color
-		 */
-		color: {
-			type: String,
-			default: 'black'
-		},
-		/** Sets background color of the button
-		 * @since 1.2.0
-		 */
-		background: {
-			type: String,
-			default: 'white'
-		},
-		/** @deprecated Use color instead */
-		oldColor: String
-	},
-	computed: {
-		styles() {
-			return {
-				'font-size': this.size,
-				color: this.color,
-				background: this.background
-			}
-		}
-	},
-	methods: {
-		handleClick(e) {
-			/** Triggered when button is clicked
-			 * @event click
-			 * @type {Event}
-			 */
-			this.$emit('click', e)
+  name: 'app-button',
+  mixins: [sizeMixin],
+  props: {
+    /**
+     * Sets the button font color
+     */
+    color: {
+      type: String,
+      default: 'black'
+    },
+    /** Sets background color of the button
+     * @since 1.2.0
+     */
+    background: {
+      type: String,
+      default: 'white'
+    },
+    /** @deprecated Use color instead */
+    oldColor: String
+  },
+  computed: {
+    styles() {
+      return {
+        'font-size': this.size,
+        color: this.color,
+        background: this.background
+      }
+    }
+  },
+  methods: {
+    handleClick(e) {
+      /** Triggered when button is clicked
+       * @event click
+       * @type {Event}
+       */
+      this.$emit('click', e)
 
-			/** Event for Alligator's example
-			 * @event gator
-			 * @type {Event}
-			 */
-			this.$emit('gator', e)
-		}
-	}
+      /** Event for Alligator's example
+       * @event gator
+       * @type {Event}
+       */
+      this.$emit('gator', e)
+    }
+  }
 }
 </script>
 
