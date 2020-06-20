9d4df45271abe104a006dc92b31ca661e79ef6bc
Kevin Kelly <kvinklly@gmail.com>
2020-06-20 16:48:28 -0500

    Add examples for different buttons

--- src/components/buttons/ButtonSmall.vue
+++ src/components/buttons/ButtonSmall.vue
@@ -30,22 +30,42 @@ export default {
   padding: 7px 14px;
 
   &.primary {
-    background: var(--primary)
+    background: var(--primary);
   }
 
   &.secondary {
-    background: var(--secondary)
+    background: var(--secondary);
+  }
+
+  &.danger {
+    background: var(--red);
+  }
+
+  &.warning {
+    background: var(--yellow);
+  }
+
+  &.success {
+    background: var(--green);
   }
 }
 </style>
 
 <docs>
-Small Button / Primary
+Small Buttons
 ```jsx
-  <ButtonSmall type="primary">Study Now</ButtonSmall>
-```
-Small Button / Secondary
-```jsx
-  <ButtonSmall type="secondary">Study Now</ButtonSmall>
+<p>Button types:</p>
+<div>
+  <ButtonSmall type="primary">Primary</ButtonSmall>
+  <ButtonSmall type="secondary">Secondary</ButtonSmall>
+  <ButtonSmall type="danger">Danger</ButtonSmall>
+  <ButtonSmall type="warning">Warning</ButtonSmall>
+  <ButtonSmall type="success">Success</ButtonSmall>
+</div>
+<p>Buttons with longer content</p>
+<div>
+  <ButtonSmall type="primary">Lorem ipsum dolor sit amet pulvinar accumsan</ButtonSmall>
+  <ButtonSmall type="primary">Sed eget faucibus purus,<br />non sagittis massa</ButtonSmall>
+</div>
 ```
 </docs>
