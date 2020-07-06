b1c654c399e8af9b47a5286f8509f9053dc54707
Kevin Kelly <kvinklly@gmail.com>
2020-07-06 10:26:52 -0500

    Add all components to main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import './assets/css/style.css'
 import ButtonSmall from './components/buttons/ButtonSmall.vue'
+import ButtonFavorite from './components/buttons/ButtonFavorite.vue'
 import ActionCard from './components/cards/ActionCard.vue'
+import StudyTask from './components/cards/StudyTask.vue'
 
-export { ButtonSmall, ActionCard }
+export { ButtonSmall, ActionCard, ButtonFavorite, StudyTask }
