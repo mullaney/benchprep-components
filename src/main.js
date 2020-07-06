dc156d9e797a8f2f5b7312e14f0570b6b82a2dae
Kevin Kelly <kvinklly@gmail.com>
2020-07-06 10:29:38 -0500

    Remove ButtonFavorite from main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import './assets/css/style.css'
 import ButtonSmall from './components/buttons/ButtonSmall.vue'
-import ButtonFavorite from './components/buttons/ButtonFavorite.vue'
 import ActionCard from './components/cards/ActionCard.vue'
 import StudyTask from './components/cards/StudyTask.vue'
 
-export { ButtonSmall, ActionCard, ButtonFavorite, StudyTask }
+export { ButtonSmall, ActionCard, StudyTask }
