82be6709275c8357541db821afc3efb39f227dea
Kevin Kelly <kvinklly@gmail.com>
2020-07-12 16:40:31 -0500

    Add storysource

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,7 @@
 const path = require('path');
 
 module.exports = {
+  addons: ['@storybook/addon-storysource'],
   stories: ['../src/**/*.stories.[tj]s'],
   webpackFinal: async (config) => {
     config.module.rules.push({
