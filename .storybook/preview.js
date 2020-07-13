e0969ccdc401595c828832a87937be9c880e7645
Kevin Kelly <kvinklly@gmail.com>
2020-07-13 08:42:34 -0500

    Add ButtonSmall back to preview.js

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -6,10 +6,12 @@ import Vue from 'vue';
 
 // Import your global components.
 import ButtonFavorite from '../src/components/buttons/ButtonFavorite.vue';
+import ButtonSmall from '../src/components/buttons/ButtonSmall.vue';
 import ActionCard from '../src/components/cards/ActionCard.vue';
 
 // Register global components.
 Vue.component('ButtonFavorite', ButtonFavorite);
+Vue.component('ButtonSmall', ButtonSmall);
 Vue.component('ActionCard', ActionCard);
 
 configure(require.context('../src', true, /\.stories\.js$/), module);
