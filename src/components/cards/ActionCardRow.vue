bfdb6e1399f34d33d573781b7bace402aeab7634
Kevin Kelly <kvinklly@gmail.com>
2020-07-15 12:59:47 -0500

    Add ActionCardRow

---
+++ src/components/cards/ActionCardRow.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="action-card-row">
+    <!-- Put Action Cards in this slot -->
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ActionCardRow'
+}
+</script>
+
+<style lang="scss">
+.action-card-row {
+  display: flex;
+  justify-content: space-around;
+}
+</style>
+
+<docs>
+```jsx
+<ActionCardRow style="background: pink">
+  <ActionCard/>
+  <ActionCard/>
+  <ActionCard/>
+</ActionCardRow>
+```
+</docs>
